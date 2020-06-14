9cce1e130c025e52e4f10179c17057de781448b3
pedro.paiva <pedro.gustavo@gmail.com>
2020-06-14 20:54:07 -0300

    Projeto curso vagrant

---
+++ vagrant/ambientes/dev/bionic/src/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Testando conexao <br /> <br />";
+$servername = "10.10.2.37";
+$username = "phpuser";
+$password = "pass";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+echo "Conectado com sucesso";
+?>
