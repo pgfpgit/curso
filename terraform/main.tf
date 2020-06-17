6939b1f047e400a344123fda497d14f44270dd6a
pedro.paiva <pedro.gustavo@gmail.com>
2020-06-17 15:24:27 -0300

    Aula 2.4 - Curso Terraform - Aplicando as mudanças na infraestrutura

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,4 +11,21 @@ resource "aws_instance" "dev" {
     tags = {
       Name = "dev${count.index}"
     }
+    vpc_security_group_ids = ["sg-0d77a126408a0f0db"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["187.181.138.99/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
 }
