17189fd0935ef4e27980f9904acdcee35e0ae5ea
pedro.paiva <pedro.gustavo@gmail.com>
2020-06-18 11:38:22 -0300

    Aula 4.4 - Curso Terraform -  Utilizando um provedor com vários recursos

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
     count = 3
     ami = "ami-085925f297f89fce1"
@@ -33,6 +39,17 @@ resource "aws_instance" "dev5" {
       Name = "dev5"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+    provider = aws.us-east-2
+    ami = "ami-026dea5602e368e96"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+      Name = "dev6"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -13,3 +13,20 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = aws.us-east-2  
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["187.181.138.99/32"]
+  }
+  
+  tags = {
+    Name = "ssh"
+  }
+}
