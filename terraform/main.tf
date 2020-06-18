353604ee8990c90fc803f6e36fb9e1b202607f94
pedro.paiva <pedro.gustavo@gmail.com>
2020-06-18 12:11:26 -0300

    Aula 4.6 - Curso Terraform - Deploy da infraestrutura multi-região

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_instance" "dev6" {
       Name = "dev6"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+    depends_on = [aws_dynamodb_table.dynamodb-homologacao]
 }
 
 resource "aws_s3_bucket" "dev4" {
@@ -58,5 +59,23 @@ resource "aws_s3_bucket" "dev4" {
 
   tags = {
     Name = "pedrolabs-dev4"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = aws.us-east-2
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
   }
 }
