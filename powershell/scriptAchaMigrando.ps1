9b7fc6e5586c7b17b4518ef00b41650bd8529373
pgfpgit <pedro.gustavo@gmail.com>
2021-09-03 16:20:37 -0300

    Aula 4 - Quebra de linha e arrays

--- powershell/scriptAchaMigrando.ps1
+++ powershell/scriptAchaMigrando.ps1
@@ -1,2 +1,2 @@
-gci -Recurse -File | Where-Object Name -like "*_migrando_*" | Select-Object Name, Length
+gci -Recurse -File | Where-Object Name -like "*_migrando_*" | Select-Object Name, { "{0:N2}KB" -f ($_.Length / 1KB) } 
 
