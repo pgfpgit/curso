79755a70d893e34b62ba48ad350e034071c508bf
pgfpgit <pedro.gustavo@gmail.com>
2021-09-05 20:01:49 -0300

    Aula 5 - Convertendo entre HTML, JSON e CSV - Exercicio Final

---
+++ powershell/ScriptsEmMigracaoExercicio.ps1
@@ -0,0 +1,40 @@
+param($tipoDeExportacao)
+$ErrorActionPreference = "Stop" 
+
+#Aqui a criamos um hashtable para a coluna do nome
+$nameExpr = @{
+    Label = "Nome";
+    Expression = { $_.Name }
+}
+
+#Tambem montamos um hashtable para o tamanho
+$lengthExpr = @{
+    Label = "Tamanho";
+    Expression = { "{0:N2}KB" -f ($_.Length / 1KB) }
+}
+
+#Criamos um array com as variáveis
+$params = $nameExpr, $lengthExpr
+
+$resultado=
+    gci -Recurse -File |
+        ? Name -like "*.exe" |
+        select $params
+If ($tipoDeExportacao -eq "HTML"){
+    $estilos = Get-Content d:\curso\powershell\styles.css
+    $styleTag = "<style> $estilos </style>"
+    $tituloPagina = "Relatorio de Scripts em Migracao"
+    $tituloBody = "<h1> $tituloPagina </h1>"
+
+    $resultado | 
+       ConvertTo-Html -Head $styleTag -Title $tituloPagina -Body $tituloBody | 
+        Out-File d:\curso\powershell\relatorioExe.html 
+} elseif ($tipoDeExportacao -eq "JSON") {
+    $resultado | 
+       ConvertTo-JSON | 
+        Out-File d:\curso\powershell\relatorioExe.json
+} elseif ($tipoDeExportacao -eq "CSV")  {
+    $resultado | 
+       ConvertTo-CSV -NoTypeInformation | 
+        Out-File d:\curso\powershell\relatorioExe.csv
+}
